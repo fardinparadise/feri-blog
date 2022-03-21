69fd58bf844449f224a5c0056107e59f2eb8cc04
Fardin Salehi <fardinparadise@gmail.com>
2022-03-21 14:30:52 +0330

    Initial Commit

--- day-69/Starting+Files+-+blog-with-users-start/main.py
+++ day-69/Starting+Files+-+blog-with-users-start/main.py
@@ -11,6 +11,8 @@ from forms import CreatePostForm, RegisterForm, LogIn, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 
+# gunicorn 20.1.0
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
